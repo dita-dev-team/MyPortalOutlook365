3c43ce20a3f0afe68325e65681a7f23f543f8af4
mtotodev05 <mtotodev05@gmail.com>
2018-06-20 15:18:29 +0300

    Initial Commit with modified views

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+@section('content')
+<div class="jumbotron">
+    <h1>My Portal Outlook 365 API</h1>
+    <p>
+        <a class="btn btn-lg btn-primary" href="/signin" role="button" id="connect-button">Connect to Outlook</a>
+    </p>
+</div>
+
+
+@endsection
